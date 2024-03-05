3bd0e4513e94f5076ffca9013f110970567a7f7d
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-03-05 20:22:23 +0530

    fixed restaurant list

--- src/utils/useRestaurants.js
+++ src/utils/useRestaurants.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 const useRestaurants = () => {
   const addressDetails = useSelector((store) => store?.address?.addressDetails);
-  console.log("Address Details: ",addressDetails)
+  console.log("Address Details: ", addressDetails);
   const lat = addressDetails?.geometry?.location?.lat;
   const lng = addressDetails?.geometry?.location?.lng;
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -15,15 +15,12 @@ const useRestaurants = () => {
   }, []);
 
   async function fetchRestaurants() {
-    const resource = generateProxyUrl(RESTAURANTS_API.replace(DEFAULT_LAT, lat).replace(DEFAULT_LNG, lng))
+    const resource = generateProxyUrl(RESTAURANTS_API.replace(DEFAULT_LAT, lat).replace(DEFAULT_LNG, lng));
     const data = await fetch(resource);
     const json = await data.json();
-    console.log("Data: ",json)
 
-    let list = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    if (list === undefined) {
-      list = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    }
+    let filteredCard = json?.data?.cards?.find((item) => item.card.card.id && item.card.card.id === "restaurant_grid_listing");
+    let list = filteredCard ? filteredCard?.card?.card?.gridElements?.infoWithStyle?.restaurants : [];
 
     setRestaurantHeader(list === undefined ? json?.data?.cards[3]?.card?.card : json?.data?.cards[2]?.card?.card);
     setListOfRestaurants(list);
